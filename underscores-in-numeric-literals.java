67adcaeee79b8e6f132511b5316cd9927e701cbf
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-08-11 16:33:06 -0700

    Add example showing the usefulness of using underscores to deliminate integer literals

---
+++ underscores-in-numeric-literals.java
@@ -0,0 +1,18 @@
+
+/*
+ * From p. 26 "Literals"
+ *
+ * """
+ * Underscores are allowed as separators between digits that denote
+ * the integer.
+ * """
+ *
+ */
+
+class Main {
+    public static void main(String[] args) {
+        int number = 1_456_234;
+        System.out.println(number);
+    }
+}
+
