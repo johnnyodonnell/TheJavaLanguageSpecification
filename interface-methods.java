c60d2e1969ba5e176eb07dda9bef03fdd757d411
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-08-11 18:21:54 -0700

    Interfaces can have default methods and thus, they can have private methods, but these should be used primarily for backward compatibility At least that is why the default methods were introduced in the first place

--- interface-methods.java
+++ interface-methods.java
@@ -19,12 +19,27 @@ interface MyInterface {
      *
      * Interface abstract methods cannot have bodies
      *
-    public void print(String message) {
+    void print(String message) {
         System.out.println(message);
     }
     */
 
-    public void print(String message);
+    // Defaults to public
+    void print(String message);
+
+    /*
+     * Inteface defaults vs Abstract class:
+     * https://stackoverflow.com/questions/19998454/interface-with-default-methods-vs-abstract-class-in-java-8
+     *
+     */
+    default void printTwo(String message) {
+        privatePrint();
+        System.out.println(message);
+    }
+
+    private void privatePrint() {
+        System.out.println("Printed from interface");
+    }
 
     public static void staticPrint(String message) {
         System.out.println(message);
@@ -41,6 +56,7 @@ class Main {
     public static void main(String[] args) {
         MyObject myObject = new MyObject();
         myObject.print("Hello World!");
+        myObject.printTwo("Hello World!");
         MyInterface.staticPrint("Hello from a static method!");
     }
 }
